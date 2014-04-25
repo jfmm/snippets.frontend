588f6b21032203849cb94758237aae8cfba32317
Jose Morales-mendizabal <moralejf@gmail.com>
2014-04-24 20:46:51 -0400

    fixed OBOB counter bug, fixed class removal method to addBack()

--- js/css-background-image-slider.js
+++ js/css-background-image-slider.js
@@ -1,4 +1,18 @@
+/*
+*=====================================================================
+CSS3 BACKGROUND IMAGE SLIDER
+
+ This code uses css3 transitions and opacity values to cycle through
+ stacked Divs with background images to create a carousel that scales with
+ the browser viewport thanks to CSS background-size: cover; property.
+
+ TODO: Add click functionality && IE8 fallback.
+*=====================================================================
+*/
+
+
 (function($) {
+
 		var slidesContainer = $('.hero');
 		var slide = $('.slide');
 		var currentSlide = 1;
@@ -6,22 +20,25 @@
 		
 
 		function cycle() {
-			//active slide must be in local scope to be updated
+			// active slide must be in local scope to be updated
 			var activeSlide = $('.active');
 
 			// as long as the slide counter is below the number of
 			// actual slides show the slides. 
-			if(currentSlide <= sliderLength) {
+			if(currentSlide < sliderLength) {
 			
-			//make upcoming div visible
+				
+			// make upcoming div visible
 			activeSlide.next().addClass('active');
 
-			//have last active slide remove its predecessors active class
-			activeSlide.prevAll().removeClass('active');
+			// have last active slide remove its predecessors active class
+			activeSlide.addBack().removeClass('active');
+			
+			// update counter variable
 			currentSlide += 1;
 			
 			} else {
-			// if counter exceeds actual slide number, reset counter
+			// if counter exceeds number of slides, reset counter,
 			// add active class to first slide and remove the active class from
 			// last element
 			currentSlide = 1;
@@ -31,8 +48,8 @@
 			}
 		}
 
-		// autoplay set
-		setInterval(function(currentSlide){cycle();}, 3000);
+		// set autoplay
+		setInterval(function(currentSlide){cycle();}, 4000);
 
 
 })(jQuery);
