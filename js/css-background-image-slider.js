251bd594723031e30cb3618d905673f2fc073f1d
Jose Morales-mendizabal <moralejf@gmail.com>
2014-04-25 15:43:48 -0400

    Added Fallback for browsers that do not support CSS Transitions

--- js/css-background-image-slider.js
+++ js/css-background-image-slider.js
@@ -6,50 +6,76 @@ CSS3 BACKGROUND IMAGE SLIDER
  stacked Divs with background images to create a carousel that scales with
  the browser viewport thanks to CSS background-size: cover; property.
 
- TODO: Add click functionality && IE8 fallback.
+ TODO: Add click functionality & Polish last IE transition
 *=====================================================================
 */
 
 
 (function($) {
-
+		var htmlDocument = $('html'); // cache document for modernizr testing
 		var slidesContainer = $('.hero');
-		var slide = $('.slide');
+		var slide = $('.slide'); //individual slide class
+		var allButFirst = $('div.slide:nth-child(n+2)'); //selects all slides but the first one in the set
 		var currentSlide = 1;
 		var sliderLength = slidesContainer.children().length;		
 		
+		// testing fallback 
+		// REMEMBER: UNCOMMENT CSS TRANSITIONS ON CSS FILES AFTER DEBUGGING
+		//htmlDocument.removeClass('csstransitions');
 
 		function cycle() {
-			// active slide must be in local scope to be updated
+			//active slide must be in local scope to be updated
 			var activeSlide = $('.active');
 
 			// as long as the slide counter is below the number of
 			// actual slides show the slides. 
 			if(currentSlide < sliderLength) {
 			
-				
-			// make upcoming div visible
-			activeSlide.next().addClass('active');
+				// test for CSS transitions using Modernizr			
+				if(htmlDocument.hasClass('csstransitions')) {	
+					console.log('we are using css3 transitions');
+					//make upcoming div visible
+					activeSlide.next().addClass('active');
 
-			// have last active slide remove its predecessors active class
-			activeSlide.addBack().removeClass('active');
+					//have last active slide remove its predecessors active class
+					activeSlide.addBack().removeClass('active');
+				} else {
+					// IE 9 & BELOW FALLBACK to jQuery Methods to handle slideshow
+					// animation.
+					console.log('we are in old browser environment');
+					
+					//make upcoming div visible and add active class to it
+					activeSlide.next().fadeTo('slow', 1).addClass('active');
+
+					//activeSlide.addBack().fadeTo('slow', 0);
+				}
 			
-			// update counter variable
+			//update counter variable
 			currentSlide += 1;
 			
 			} else {
 			// if counter exceeds number of slides, reset counter,
-			// add active class to first slide and remove the active class from
-			// last element
+			// fade in first slide in the set first, and then fade out the last one
+			// in the set to reveal the first one. 
 			currentSlide = 1;
-			slide.first().addClass('active');
-			slide.last().removeClass('active');
-			
+
+				// again, check for CSS transitions browswer support			
+				if(htmlDocument.hasClass('csstransitions')) {
+					slide.first().addClass('active');
+					slide.last().removeClass('active');
+				} else {
+					//IE 9 & Below Fallback
+
+					// TODO: good solution but a bit weird laggy at the end.
+					// it eliminates inline opacity of all slides but the first one and gets
+					// rid of the active class.
+					allButFirst.fadeTo(400, 0).removeClass('active');
+		
+				}
 			}
 		}
 
-		// set autoplay
+		// autoplay set
 		setInterval(function(currentSlide){cycle();}, 4000);
-
 
 })(jQuery);
