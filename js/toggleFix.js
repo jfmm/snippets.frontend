57fc09f276273f3a209b18234dcea33a2c1b4091
jfmm <moralejf@gmail.com>
2014-08-01 11:58:42 -0400

    Class Toggling Completed

--- js/toggleFix.js
+++ js/toggleFix.js
@@ -1,4 +1,13 @@
-var affixedSidebar = $("#affixed-sidebar");
+/*==========================================
+toggleFix.js by Jose Morales Mendizabal
+This Snippet of code imitates Bootstrap's
+Affix.js.
+
+Dependencies: jQuery
+===========================================*/
+
+
+			var affixedSidebar = $("#affixed-sidebar");
 			var topOffset = $('.jumbotron').outerHeight(true); // 400
 			var bottomOffset = $(document).height() - ($("footer").outerHeight(true) + 900);
 
@@ -12,22 +21,31 @@ var affixedSidebar = $("#affixed-sidebar");
 				
 				var window = $(this);
 				
-				console.log(window.scrollTop()); // log the window position for debugging
+				
+				// if window position is past the jumbotron but not past the footer fix the nav
 				if(window.scrollTop() > topOffset && window.scrollTop() < bottomOffset) {
 					
-					console.log("entering affixed zone, nav is fixed");
-					affixedSidebar.removeClass("custom-affixed-neutral");
-					affixedSidebar.addClass("custom-affixed");
+						console.log("entering affixed zone, nav is fixed");
+						affixedSidebar.removeClass("toggle-static");
+						affixedSidebar.removeClass("toggle-bottom");
+						affixedSidebar.addClass("toggle-affixed");
+						affixedSidebar.removeAttr("style");
 					
 				} else {
-						if (window.scrollTop() >= bottomOffset) {
-							console.log("nav is normal, entering bottom");
-							affixedSidebar.removeClass("custom-affixed");
-							affixedSidebar.addClass("custom-affixed-neutral");
+						
+						if (window.scrollTop() >= bottomOffset) 
+						{
+								console.log("nav should be in bottom position");
+								affixedSidebar.removeClass("toggle-affixed");
+								affixedSidebar.removeClass("toggle-static");
+								affixedSidebar.addClass("toggle-bottom");
+								affixedSidebar.css("top", 2000); 
 						}
-					console.log("nav is normal");
-					affixedSidebar.removeClass("custom-affixed");
-					affixedSidebar.addClass("custom-affixed-neutral");
-					
+						else if (window.scrollTop() < topOffset) 
+						{
+								console.log("nav should be static");
+								affixedSidebar.removeClass("toggle-affixed");
+								affixedSidebar.addClass("toggle-static");
+						}
 				} 
 			});
